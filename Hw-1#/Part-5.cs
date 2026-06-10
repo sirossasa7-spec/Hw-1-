ea29c3a73285f0ea81548a8568d23a0fcc1e3e60
sirossasa7@gmail.com <sirossasa7@gmail.com>
2026-06-10 19:22:00 +0200

    Part-5

---
+++ Hw-1#/Part-5.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Hw_1_
+{
+    internal class Part_5
+    {
+        static void Main()
+        {
+            Console.Write("Введіть вираз: ");
+            string expr = Console.ReadLine().Replace(" ", "");
+
+            int result = 0;
+            string number = "";
+            char operation = '+';
+
+            foreach (char c in expr)
+            {
+                if (char.IsDigit(c))
+                {
+                    number += c;
+                }
+                else
+                {
+                    int value = int.Parse(number);
+
+                    if (operation == '+')
+                        result += value;
+                    else
+                        result -= value;
+
+                    operation = c;
+                    number = "";
+                }
+            }
+
+            int last = int.Parse(number);
+
+            if (operation == '+')
+                result += last;
+            else
+                result -= last;
+
+            Console.WriteLine($"Результат = {result}");
+        }
+    }
+}
